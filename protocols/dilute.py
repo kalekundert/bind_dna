c8735a548b9d6feaa6e843208123c9a8ae590d6a
Kale Kundert <kale@thekunderts.net>
2020-02-25 20:00:55 -0500

    Read the target concentration from the database.

--- protocols/dilute.py
+++ protocols/dilute.py
@@ -20,11 +20,13 @@ Arguments:
         An individual ng/µL measurement.
 
 Options:
-    -c --conc <nM>  [default: 75]
-        The concentration achieve after dilution.  The default is 75 nM, which 
-        corresponds to 125 ng/μL of the DHFR control template.  
+    -c --conc <nM>
+        The concentration achieve after dilution.  The default is read from the 
+        "Conc" column of the database containing the given species, or 75 nM if 
+        no such concentration is specified.
 
         Note: 1 pmol/µL is 1000 nM.
+        Note: 75 nM corresponds to 125 ng/μL of the DHFR control template.  
 
     -v --volume <uL>  [default: 20]
         The volume of diluted amplicon to make.
@@ -41,9 +43,20 @@ import stepwise
 eval_or_none = lambda x: x if x is None else eval(x)
 
 args = docopt.docopt(__doc__)
-final_nM = eval(args['--conc'])
 final_uL = eval(args['--volume'])
 stock_uL = eval_or_none(args['--stock-volume'])
+
+def get_type(tag):
+    try:
+        return dbp.get_cols(tag)['Type']
+    except:
+        return 'DNA'
+
+def get_final_conc(tag):
+    if args['--conc']:
+        return eval(args['--conc']) 
+    else:
+        return dbp.get_conc(tag)
 
 if tsv := args['<tsv>']:
     nanodrop = pd.read_csv(tsv, sep='\t')
@@ -57,21 +70,39 @@ else:
         'stock_ng_uL':  float(args['<ng_uL>']),
     }])
 
+df['type'] = df['amplicon'].apply(get_type)
 df['mw_da'] = df['amplicon'].apply(dbp.get_mw)
 df['stock_nM'] = 1e6 * df['stock_ng_uL'] / df['mw_da']
+df['final_nM'] = df['amplicon'].apply(get_final_conc)
 
 if stock_uL is not None:
     df['stock_uL'] = stock_uL
-    df['water_uL'] = stock_uL * (df['stock_nM'] / final_nM - 1)
+    df['water_uL'] = stock_uL * (df['stock_nM'] / df['final_nM'] - 1)
 else:
-    df['stock_uL'] = final_uL * final_nM / df['stock_nM']
+    df['stock_uL'] = final_uL * df['final_nM'] / df['stock_nM']
     df['water_uL'] = final_uL - df['stock_uL']
 
 pd.set_option('display.precision', 2)
 
+types = '/'.join(df['type'])
+if len(x := set(df['final_nM'].dropna())) == 1:
+    final_nM = f' to {x.pop():.2f} nM'
+else:
+    final_nm = ''
+
+df = df[[
+        'amplicon',
+        'mw_da',
+        'stock_ng_uL',
+        'stock_nM',
+        'final_nM',
+        'stock_uL',
+        'water_uL',
+]]
+
 protocol = stepwise.Protocol()
 protocol += f"""\
-Dilute the purified DNA to {final_nM:.2f} nM:
+Dilute the purified {types}{final_nM}:
 
 {df}
 """
