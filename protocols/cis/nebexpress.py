689ec56f97af6dd294d2666d22764e511ced0705
Kale Kundert <kale@thekunderts.net>
2020-03-09 21:20:34 -0400

    Add incubation time/temp and template conc options.

--- protocols/cis/nebexpress.py
+++ protocols/cis/nebexpress.py
@@ -4,12 +4,22 @@
 Express proteins from linear DNA templates using NEBExpress.
 
 Usage:
-    nebexpress.py <templates>
+    nebexpress.py <templates> [options]
 
 Arguments:
     <templates>
         Comma-separated list of templates.  The number of reactions will be 
         inferred from this list.
+
+Options:
+    -d --template-stock <nM>             [default: 75]
+        The stock concentration of the template DNA, in units of nM.
+
+    -t --incubation-time <time>         [default: 2-4 hours]
+        The amount of time to incubate the reactions.
+
+    -T --incubation-temperature <temp>  [default: 37°C]
+        The temperature to incubate the reactions at.
 """
 
 import docopt
@@ -21,19 +31,21 @@ templates = args['<templates>']
 num_templates = len(templates.split(','))
 
 rxn = stepwise.MasterMix.from_text("""\
-Reagent                       Stock    Volume  MM?
-========================  =========  ========  ===
-water                                to 50 µL  yes
-S30 extract                             12 µL  yes
-synthesis buffer                 2x     25 µL  yes
-T7 RNA polymerase          450 U/µL      1 µL  yes
-RNase inhibitor (murine)    40 U/µL      1 µL  yes
-linear DNA template       125 ng/µL      2 µL
-GamS nuclease inhibitor                  1 µL  yes
+Reagent                            Stock    Volume  MM?
+=============================  =========  ========  ===
+water                                     to 50 µL  yes
+S30 extract [2]                              12 µL  yes
+synthesis buffer [2]                  2x     25 µL  yes
+T7 RNA polymerase               450 U/µL      1 µL  yes
+RNase inhibitor (murine)         40 U/µL      1 µL  yes
+GamS nuclease inhibitor [2,3]  1.5 µg/µL      1 µL  yes
+linear DNA template                75 nM      2 µL
 """)
 rxn.hold_ratios.volume = '10 µL'
 rxn.num_reactions = num_templates
-rxn['linear DNA template'].name = templates
+rxn['linear DNA template'].name = f'{templates} [3]'
+rxn['linear DNA template'].hold_conc.stock_conc = (
+        float(args['--template-stock']), 'nM')
 
 protocol = stepwise.Protocol()
 
@@ -48,7 +60,7 @@ Setup {plural(rxn.num_reactions):# NEBExpress reaction/s} [1]:
 """
 
 protocol += f"""\
-Incubate at 37°C for 2-4 hours [2].
+Incubate at {args['--incubation-temperature']} for {args['--incubation-time']} [4].
 """
 
 protocol.footnotes[1] = """\
@@ -59,6 +71,15 @@ before RecBCD has a chance to act on the DNA.
 """
 
 protocol.footnotes[2] = """\
+Aliquot to avoid multiple freeze/thaw cycles.
+"""
+
+protocol.footnotes[3] = """\
+Optimal concentration must be determined 
+empirically for each template.
+"""
+
+protocol.footnotes[4] = """\
 Additional incubation time (maximum 10 hours) at 
 37°C may increase yield.
 """
