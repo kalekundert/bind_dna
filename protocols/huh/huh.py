1a72eb6d45fa2ee6030c8afb76614efc59b486a1
Kale Kundert <kale@thekunderts.net>
2021-07-22 16:54:12 -0400

    Make the HUH-tagging protocol general to any protein.

--- protocols/huh/huh.py
+++ protocols/huh/huh.py
@@ -10,12 +10,12 @@ Arguments:
     <protein>
         The names of 1 or more proteins to attach to the below DNA sequence(s).
     <dna>
-        The names of 1 or more DNA sequences to attach to the above protein(s).  These names 
-        should be present in the FreezerBox database and have associated stock 
-        concentrations and molecular weights.  Note that FreezerBox can't 
-        automatically calculate molecular weights for non-standard nucleotides 
-        like iSP9, so you may need to provide a molecular weight manually for 
-        such constructs.
+        The names of 1 or more DNA sequences to attach to the above protein(s).  
+        These names should be present in the FreezerBox database and have 
+        associated stock concentrations and molecular weights.  Note that 
+        FreezerBox can't automatically calculate molecular weights for 
+        non-standard nucleotides like iSP9, so you may need to provide a 
+        molecular weight manually for such constructs.
 
 Options:
     -n --num-reactions <int>
@@ -26,25 +26,27 @@ Options:
         Which buffer to use.  The default is the buffer from [VegaRocha2007].  
         The buffer is assumed to be 10x.
 
+    -C --buffer-conc <x>            [default: 10]
+        The concentration of the buffer, e.g. 10x.
+
     -B --buffer-divalent-conc <mM>  [default: 2.5]
         The concentration of divalent cation in the buffer, in mM.  This is 
         used to calculate the appropriate concentration of EDTA to add to the 
         negative control.
 
-    -c --cas9-stock-conc <µM>       [default: 22]
-        The stock concentration of dCas9-PCV2, in µM.
+    -P --protein-stock-conc <µM>       [default: 32]
+        The stock concentration of the PCV2 fusion protein, in µM.
 
-    -S --no-sgrna
-        Exclude sgRNA from the reaction.
-
-    -m --master-mix <reagents>      [default: cas9,sgrna,buffer]
+    -m --master-mix <reagents>      [default: protein,buffer]
         A comma-separated list of reagents to include in the master mix.  The 
-        following reagents are understood: cas9, sgrna, dna, edta, buffer
+        following reagents are understood: protein, dna, edta, buffer
 """
 
 import docopt
 import stepwise
 import freezerbox
+
+from stepwise import pl, ul
 
 # This script should be broken in two: one for proteins in general and another 
 # for Cas9.  The only real difference for Cas9 is that the reaction includes 
@@ -58,49 +60,42 @@ Reagent      Stock    Volume  MM?
 ==========  ======  ========  ===
 water               to 10 µL  yes
 buffer         10x      1 µL  yes
-dCas9-PCV2    1 µM      1 µL  yes
-sgRNA         1 µM      1 µL  yes
+protein       1 µM      1 µL  yes
 EDTA        500 mM      1 µL
 DNA         200 nM      5 µL
 """)
 
 db = freezerbox.load_db()
 
-dna = args['<dna>']
+dna = args['<dna>'].split(',')
 dna_nM = min([round(db[x].conc_nM) for x in dna])
 
 huh.num_reactions = int(args['--num-reactions'] or len(dna))
 huh.extra_min_volume = '0.5 µL'
 
 huh['buffer'].name = args['--buffer']
-huh['dCas9-PCV2'].hold_conc.stock_conc = args['--cas9-stock-conc'], 'µM'
-huh['sgRNA'].hold_conc.stock_conc = 10, 'µM'
+huh['buffer'].hold_conc.stock_conc = args['--buffer-conc'], 'x'
+huh['protein'].name = args['<protein>']
+huh['protein'].hold_conc.stock_conc = args['--protein-stock-conc'], 'µM'
 huh['EDTA'].hold_stock_conc.conc = 30 / 2.5 * float(args['--buffer-divalent-conc']), 'mM'
 huh['DNA'].hold_conc.stock_conc = dna_nM, 'nM'
 huh['DNA'].name = ','.join(dna)
 
 mm = args['--master-mix'].split(',')
-huh['dCas9-PCV2'].master_mix = 'cas9' in mm
-huh['sgRNA'].master_mix = 'sgrna' in mm
+huh['protein'].master_mix = 'protein' in mm
 huh['DNA'].master_mix = 'dna' in mm
 huh['EDTA'].master_mix = 'edta' in mm
 huh['buffer'].master_mix = 'buffer' in mm
 
-if args['--no-sgrna']:
-    del huh['sgRNA']
-else:
-    p += stepwise.load('zap')
-    huh['water'].name = 'nuclease-free water'
-
-p += f"""\
-Attach DNA to HUH-tagged dCas9 [1-3]:
-
-{huh}
-
-- Add each reagent in order.
-- Mix after adding EDTA (and before adding DNA).
-- Incubate at 37°C for 15 min.
-"""
+p += pl(
+        "Attach DNA to PCV2 [1-3]:",
+        huh,
+        ul(
+            "Add each reagent in order.",
+            "Mix after adding EDTA (and before adding DNA).",
+            "Incubate at 37°C for 15 min.",
+        ),
+)
 
 # 500 ng/band
 p.footnotes[1] = """\
@@ -128,4 +123,5 @@ want a lot of unbound DNA in my qPCR reactions and
 because a 10:1 ratio would use a lot of material.
 """
 
-print(p)
+if __name__ == '__main__':
+    p.print()
