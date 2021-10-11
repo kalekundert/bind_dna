a7cb717b9e321c9fbca126e31753ec4c2f7dc6f3
Kale Kundert <kale@thekunderts.net>
2021-10-11 13:30:01 -0400

    Update the B1H plate assay

--- protocols/b1h/plate_assay.py
+++ protocols/b1h/plate_assay.py
@@ -14,6 +14,7 @@ Arguments:
 
 import docopt
 import stepwise
+from stepwise import pl, ul, table
 
 args = docopt.docopt(__doc__)
 p = stepwise.Protocol()
@@ -26,32 +27,29 @@ additives = [
         ['0 mM', '4 mM'],
 ]
 
-p += f"""\
-Make NM plates with the following additives:
+p += pl(
+        "Make NM plates with the following additives:",
+        table(additives[1:], header=additives[0]),
+)
 
-{stepwise.tabulate(additives, True)}
-"""
+p += (
+        f"Pick fresh colonies of the following strains and grow "
+        f"to OD≈1 in 3 mL LB+Carb+Kan: {','.join(args['<strains>'])}"
+)
 
-p += f"""\
-Pick fresh colonies of the following strains and 
-grow to OD≈1 in 3 mL LB+Carb+Kan: {','.join(args['<strains>'])}
-"""
+p += pl(
+        "Wash cells with minimal media:",
+        ul(
+            "Spin 3500g, 3 min, 4°C.",
+            "Resuspend in 1 mL NM.",
+            "Spin 3500g, 3 min, 4°C.",
+            "Resuspend in 1 mL NM.",
+            "Dilute to OD=0.1",
+        ),
+)
 
-p += """\
-Wash cells with minimal media:
+p += stepwise.load('serial 90µL 0.1OD / 10 6 -m cells -d NM')
 
-- Spin 3500g, 3 min, 4°C.
-- Resuspend in 1 mL NM.
-- Spin 3500g, 3 min, 4°C.
-- Resuspend in 1 mL NM.
-- Dilute to OD=0.1
-"""
+p += "Plate 5 µL of each dilution in triplicate on each plate."
 
-p += stepwise.load('serial 90µL 0.1OD 6 -f 10 -m cells -d NM')
-
-p += """\
-Plate 5 µL of each dilution in triplicate on each 
-plate.
-"""
-
-print(p)
+p.print()
